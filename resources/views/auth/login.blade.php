cc2df6a74c4e24d6c3ea0f00ef94e704ab2bc1f8
michael101136 <michael101136@gmail.com>
2017-12-28 11:11:56 -0500

    nueva interface
    nueva interface

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,42 @@
-@extends('layoutBack')
+@extends('layoutLogin')
 
 @section('contenido')
-     
-        <div style="margin: 0px;">
+	<div class="login-box">
+			 <div class="login-box-body">
+					 <div class="login-logo">
+					    	<b>Ingrese sus datos</b>
+					  </div>
+						<form method="POST" action="login">
+			        				{!!  csrf_field() !!}
+								<div class="form-group">
+										<label for="name" class="cols-sm-1 control-label">Email</label>
+										<div class="cols-sm-1">
+											<div class="input-group">
+												<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+												<input type="email" name="email"  placeholder="Email" class="form-control">
+											</div>
+										</div>
+								</div>
+								<div class="form-group">
+										<label for="name" class="cols-sm-1 control-label">Contraseña</label>
+										<div class="cols-sm-1">
+											<div class="input-group">
+												<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+												<input type="password" name="password" placeholder="Contraseña" class="form-control">
+											</div>
+										</div>
+								</div>
+								<div class="form-group" style="margin-left: 150px;">
+										<div class="cols-sm-2">
+											<div class="input-group">
+												<input type="submit" value="Iniciar" class="btn btn-info btn-md">
+											</div>
+										</div>
+								</div>
+			      		</form>			 	
 
-		       
-	     	<div class="panel panel-default">
-			  <div class="panel-heading">Ingrese sus datos</div>
-				  <div class="panel-body">
 
-						<div class="container" >
-                           
-                          	<div class="row">
-                          		<div class="col-md-4" style="float: center;">
-                          		</div>
-                                <div class="col-md-4" style="float: center;background: #e3e3e3;">
-									  	 <form method="POST" action="login">
-							        				{!!  csrf_field() !!}
-										<div class="form-group">
-												<label for="name" class="cols-sm-1 control-label">Email</label>
-												<div class="cols-sm-1">
-													<div class="input-group">
-														<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-														<input type="email" name="email"  placeholder="Email" class="form-control">
-													</div>
-												</div>
-										</div>
-										<div class="form-group">
-												<label for="name" class="cols-sm-1 control-label">Contraseña</label>
-												<div class="cols-sm-1">
-													<div class="input-group">
-														<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-														<input type="password" name="password" placeholder="Contraseña" class="form-control">
-													</div>
-												</div>
-										</div>
-										<div class="form-group" style="margin-left: 150px;">
-												<div class="cols-sm-2">
-													<div class="input-group">
-														<input type="submit" value="Iniciar" class="btn btn-info btn-md">
-													</div>
-												</div>
-										</div>
-						      		</form>
-							      	<div class="col-md-4" style="float: center;">
-	                          		</div>
-						      	</div>
-						     </div>
-						 </div>
-			      		<br/>
-				  </div>
-			</div>
-	     </div>
-	    
+		    </div>
+	</div>
+		    
 @stop
---
+++ resources/views/layoutLogin.blade.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Spaziour.com</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <script type="text/javascript" src="{{ asset("js/jquery.js") }}"></script>
+        <script type="text/javascript" src="{{ asset("js/bootstrap.js") }}"></script>
+        <link rel="stylesheet"  href="{{ asset("css/app.css") }}"> 
+        <link rel="stylesheet" href="{{ asset("AdminLT/css/bootstrap.min.css") }}">
+        <link rel="stylesheet" href="{{ asset("AdminLT/css/font-awesome.min.css") }}">
+        <link rel="stylesheet" href="{{ asset("AdminLT/css/ionicons.min.css") }}">
+        <link rel="stylesheet" href="{{ asset("AdminLT/css/AdminLTE.min.css") }}">
+        <link rel="stylesheet" href="{{asset("AdminLT/css/skins/skin-blue.min.css") }}">
+
+    </head>
+    
+<body class="hold-transition skin-blue sidebar-mini">
+    <?php function activeMenu($url)
+                {
+                    return request()->is($url) ? 'active' : '';
+                } ?>
+<div class="wrapper">
+
+  <header class="main-header">
+    <a href="index2.html" class="logo">
+      <span class="logo-mini"><b>A</b>LT</span>
+      <span class="logo-lg"><b>ProDU</b>CE</span>
+    </a>
+
+    <nav class="navbar navbar-static-top" role="navigation">
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li class="dropdown tasks-menu">
+             <div class="collapse navbar-collapse navbar-ex1-collapse">
+                            <ul class="nav navbar-nav">
+                            
+
+                                @if(auth()->check())
+                                    
+                                        <li class= "{{ activeMenu('mensajes') }}">
+                                            <a  href="{{ route('mensajes.index')}}">mensajes</a>
+                                        </li>
+                                        @if(auth()->user()->hasRoles(['admin','estudiante']))
+                                                 <li class= "{{ activeMenu('usuarios') }}"> 
+                                                        <a  href="{{ route('usuarios.index')}}">usuarios</a>
+                                                 </li>
+                                        @endif
+
+                                           
+
+                                @endif
+
+                            </ul>
+
+                            </li>
+                                 <ul class="nav navbar-nav navbar-left">
+                                    @if(auth()->guest())
+
+                                        <li class= "{{ activeMenu('login') }}">
+                                            <a  href="{{ URL::to('login') }}">login</a> 
+                                        </li> 
+
+                                    @else 
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ auth()->user()->name }} <b class="caret"></b></a>
+                                            <ul class="dropdown-menu">
+                                                <li><a href="logout"> cerrar </a></li>
+                                            </ul>
+                                        </li>
+
+                                    @endif
+
+                                
+              </ul>
+          <li>
+            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <div class="content-wrapper">
+    
+    <section class="content-header">
+       @yield('contenido')
+    </section>
+
+    <section class="content container-fluid">
+
+    </section>
+  </div>
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+    </div>
+     Copyrigth {{ date('y')}}
+  </footer>
+
+</div>
+
+
+<script src="{{asset("AdminLT/jquery/jquery.min.js") }}"></script>
+<script src="{{asset("AdminLT/js/bootstrap.min.js") }}"></script>
+<script src="{{asset("AdminLT/js/adminlte.min.js") }}"></script>
+
+</body>
+
+</html>
